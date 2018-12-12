b99416d8c71d741ccfc0865fa000903a7d8ebac3
d3f38 <37728791+d3f38@users.noreply.github.com>
2018-12-12 12:16:56 +0300

    Add files via upload

--- works/feeback-form/js/main.js
+++ works/feeback-form/js/main.js
@@ -7,13 +7,15 @@ jQuery(document).ready(function($) {
     )
     $('.thanks').click(function (){
         $('.form-thanks').css('display', 'block');
-    }
-)
+    });
     $('.close-button').click(function (){
         $('.form-feedback').css('display', 'none');
         $('.form-thanks').css('display', 'none');
-    }
-)
+    });
+    $('#buttonClose').click(function (){
+        $('.form-feedback').css('display', 'none');
+    });
+    
 
 
     // Добавляем маску для поля с номера телефона
@@ -22,9 +24,11 @@ jQuery(document).ready(function($) {
         if($(this).val() != '') {
             $(this).css({'border' : '1px solid #7fbd87'});
             $('#check-4').css('display', 'block');
+            $('.valid-phone').text('');
         } else {
-            // $(this).css({'border' : '1px solid #9f0000'});
+            $(this).css({'border' : '1px solid #9f0000'});
             $('#check-4').css('display', 'none');
+            $('.valid-phone').text('Неправильно заполенное поле');
         }
     });
 
@@ -44,14 +48,24 @@ jQuery(document).ready(function($) {
             if(pattern.test($(this).val())){
                 $(this).css({'border' : '1px solid #7fbd87'});
                 $('#check-3').css('display', 'block');
-                $('#valid').text('');
+                $('.valid-email').text(''); 
             } else {
                 $(this).css({'border' : '1px solid #9f0000'});
                 $('#check-3').css('display', 'none');
-                $('#valid').text('Неправильно заполенное поле');
+                $('.valid-email').text('Неправильно заполенное поле');      $('#form-label-emeil').css('position','absolute');
+                $('#form-label-emeil').css('top','-15px');
+                $('#form-label-emeil').css('left','0');
+                $('#form-label-emeil').css('font-size','10px'); 
             }
         } else {
-            // $(this).css({'border' : '1px solid #9f0000'});
+            $(this).css({'border' : '1px solid #9f0000'});
+            $('.valid-email').text('Неправильно заполенное поле');
+            
+            // $('.field:not(:focus):valid~.form-label').css('position','absolute');
+            // $('.field:not(:focus):valid~.form-label').css('top','-15px');
+            // $('.field:not(:focus):valid~.form-label').css('left','0');
+            // $('.field:not(:focus):valid~.form-label').css('font-size','10px');
+
         }
     });
     //Проверка имени
@@ -59,9 +73,11 @@ jQuery(document).ready(function($) {
         if($(this).val() != '') {
             $(this).css({'border' : '1px solid #7fbd87'});
             $('#check-1').css('display', 'block');
+            $('.valid-name').text('');
         } else {
-            // $(this).css({'border' : '1px solid #9f0000'});
+            $(this).css({'border' : '1px solid #9f0000'});
             $('#check-1').css('display', 'none');
+            $('.valid-name').text('Неправильно заполенное поле');
         }
     });
     //Проверка должности
@@ -69,9 +85,11 @@ jQuery(document).ready(function($) {
         if($(this).val() != '') {
             $(this).css({'border' : '1px solid #7fbd87'});
             $('#check-2').css('display', 'block');
+            $('.valid-position').text('');
         } else {
-            // $(this).css({'border' : '1px solid #9f0000'});
+            $(this).css({'border' : '1px solid #9f0000'});
             $('#check-2').css('display', 'none');
+            $('.valid-position').text('Неправильно заполенное поле');
         }
     });
     //Проверка текста
@@ -79,9 +97,11 @@ jQuery(document).ready(function($) {
         if($(this).val() != '') {
             $(this).css({'border' : '1px solid #7fbd87'});
             $('#check-5').css('display', 'block');
+            $('.valid-message').text('');
         } else {
-            // $(this).css({'border' : '1px solid #9f0000'});
+            $(this).css({'border' : '1px solid #9f0000'});
             $('#check-5').css('display', 'none');
+            $('.valid-message').text('Неправильно заполенное поле');
         }
     });
 
